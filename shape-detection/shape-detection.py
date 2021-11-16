b4981dff89a3e0443c74a27b37a9aeaadad3f9f8
Kacper Cierzniewski <kacper.cierzniewski@synergycodes.com>
2021-11-16 12:25:45 +0100

    update processing params and contours details info

--- shape-detection/shape-detection.py
+++ shape-detection/shape-detection.py
@@ -9,6 +9,7 @@ capture = cv2.VideoCapture(0)
 capture.set(3, frameWidth)
 capture.set(4, frameHeight)
 
+IMG_URL = "all shapes with bg.jpg"
 
 
 def noop(_):
@@ -39,32 +40,37 @@ def dilateImage(img):
     kernel = np.ones((5,5))
     return cv2.dilate(img, kernel, iterations=1)
     
-def drawContours():
-    img = processImage(getImageFromPath('free connection with arrow.jpg'))
-    imgContour = getImageFromPath('free connection with arrow.jpg').copy();
+def drawContours(IMG_URL):
+    img = processImage(getImageFromPath(IMG_URL))
+    imgContour = getImageFromPath(IMG_URL).copy();
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(imgContour, contours, -1, (255,0,255), 7)
-    cv2.imshow("stick test", imgContour)
+    cv2.drawContours(imgContour, contours, -1, (0,255,0), 7)
     for cnt in contours:
-        cv2.drawContours(imgContour, cnt, -1, (255,0,255), 7)
+        # cv2.drawContours(imgContour, cnt, -1, (255,0,255), 7)
         peri = cv2.arcLength(cnt, True)
-        area = cv2.contourArea(cnt);
         approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
-        print(len(approx))
+        area = cv2.contourArea(cnt);
+        x,y,w,h = cv2.boundingRect(approx)
+        cv2.putText(imgContour, "Points:" + str(len(approx)), (x ,y), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,0,0), 1)
+        cv2.putText(imgContour, "Area:" + str(int(area)), (x ,y + 25) , cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,0,0), 1)
+        cv2.putText(imgContour, "Position:x= " + str(x), (x ,y + 50)  , cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,0,0), 1)
+        cv2.putText(imgContour, "Position:y= " + str(y), (x ,y + 75)  , cv2.FONT_HERSHEY_COMPLEX, 0.7, (0,0,0), 1)
+    cv2.imshow(IMG_URL + " stick with contours", imgContour)
 
 
 cv2.namedWindow("Thresholds")
 cv2.resizeWindow("Thresholds",600,400)
-cv2.createTrackbar("Threshold1","Thresholds", 80,255,noop)
-cv2.createTrackbar("Threshold2","Thresholds", 80,255,noop)
+cv2.createTrackbar("Threshold1","Thresholds", 176,255,noop)
+cv2.createTrackbar("Threshold2","Thresholds", 118,255,noop)
 
 while True:
-    success, img = capture.read()
+    # success, img = capture.read()
     # cv2.imshow("Video", processImage(img))
     # cv2.imshow("Circles", processImage(getImageFromPath('circles.jpg')))
-    # cv2.imshow("free connection with arrow", processImage(getImageFromPath('free connection with arrow.jpg')))
+    cv2.imshow(IMG_URL + " processed", processImage(getImageFromPath(IMG_URL)))
+    cv2.imshow(IMG_URL, getImageFromPath(IMG_URL))
     # cv2.imshow("free connection with line", processImage(getImageFromPath('free connection with line.jpg')))
     # cv2.imshow("stick connections", processImage(getImageFromPath('stick connections.jpg')))
-    drawContours()
+    drawContours(IMG_URL)
     if(cv2.waitKey(1) & 0xFF == ord('q')):
         break
